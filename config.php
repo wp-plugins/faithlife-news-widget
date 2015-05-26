f750660f5f821a6f5522205acbbd53de40a7a61d
mejordan <mejordan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-26 15:56:57 +0000

    tagging version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/faithlife-news-widget/tags/1.0.1@1167812 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- config.php
+++ config.php
@@ -1,21 +1,21 @@
 <?php
 	
-return [
-	'image_path' => [
+return array(
+	'image_path' => array(
 		's3' => 'http://fln.s3.amazonaws.com/img/',
 		'placeholder' => 'https://news.faithlife.com/images/source-placeholders/'
-	],
-	'api' => [
+	),
+	'api' => array(
 		'key' => '1234',
 		'sources' => 'https://news.faithlife.com/api/plugin_getsources.php',
 		'articles' => 'https://news.faithlife.com/api/plugin_getarticles.php'
-	],
-	'app' => [
+	),
+	'app' => array(
 		'url' => 'https://news.faithlife.com',
 		'article_path' => 'https://news.faithlife.com/article/',
 		'tracking' => 'utm_source=' . urlencode(CleanUrl(home_url())) . '&utm_medium=blog&utm_campaign=fln-widget',
-	]
-];
+	)
+);
 
 function CleanUrl($url) {
 	$url = str_replace('http://', '', $url);
